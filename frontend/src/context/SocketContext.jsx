1b35cc1c116be68da3c5976d4361c3cde9f19e47
Hadi <hadiyakhadija62@gmail.com>
2024-12-14 00:04:06 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,8 +15,8 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			// const socket = io("https://chat-app-yt.onrender.com", {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chat-app-prod-ae4i.onrender.com", {
+			// const socket = io("http://localhost:5000", {
 				
 				query: {
 					userId: authUser._id,
